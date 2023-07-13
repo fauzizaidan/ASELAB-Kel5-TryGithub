9c2c5ebf50a890298f302655e773f5e3907e6c28
fauzizaidan <136500434+fauzizaidan@users.noreply.github.com>
2023-07-13 14:25:20 +0700

    Add files via upload

---
+++ Meet4try.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello guys, kami dari kelompok 5, perkenalkan dikelompok kami ada")
+	fmt.Println("Fauzi Zaidan Pratama(1301223053)")
+	fmt.Println("Fauzein Mulya Warman(1302223127)")
+	fmt.Println("Fikri Fauzi(1301223043)")
+	fmt.Println("Habli Zulvana Ath-Thaariq(1302210024)")
+	fmt.Println("Hafidz Hito Baihaqi(1301223450)")
+	fmt.Println("HAFIZ ALKHAIRI RENAL(1301223351)")
+}
